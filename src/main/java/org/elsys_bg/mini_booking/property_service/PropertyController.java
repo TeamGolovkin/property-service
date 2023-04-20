08b53371df068cd13f37171ba667dc7310873503
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2023-04-20 18:46:06 +0000

    Finalize database structure

--- src/main/java/org/elsys_bg/mini_booking/property_service/PropertyController.java
+++ src/main/java/org/elsys_bg/mini_booking/property_service/PropertyController.java
@@ -1,11 +1,10 @@
 package org.elsys_bg.mini_booking.property_service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.cassandra.core.CassandraOperations;
-import org.springframework.data.cassandra.core.query.Query;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,18 +16,28 @@ public class PropertyController {
 	@Autowired
 	private CassandraOperations cassandraTemplate;
 
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+
 	@GetMapping("/properties")
-	public List<Property> getProperties() {
-		List<Property> properties = cassandraTemplate.query(Property.class).all();
+	public List<PropertyOfUser> getProperties() {
+		List<PropertyOfUser> properties = cassandraTemplate.query(PropertyOfUser.class).all();
 		return properties;
 	}
 
 	@PostMapping("/property")
 	public void addProperty() {
-		Property peshoMansoin = new Property(69, "us");
-		cassandraTemplate.insert(peshoMansoin);
+		PropertyOfUser peshoMansion = new PropertyOfUser(1,
+														 1,
+														 "pesh manshon",
+														 "us za isus",
+														 69);
+		cassandraTemplate.insert(peshoMansion);
+	}
 
-		Property queriedPesho = cassandraTemplate.selectOneById(69, Property.class);
-		System.out.println(queriedPesho);
+	@PostMapping("/request")
+	public void requestProperty() {
+		kafkaTemplate.send("requested", "Pesho iska kushta");
 	}
 }
---
+++ src/main/java/org/elsys_bg/mini_booking/property_service/PropertyOfUser.java
@@ -0,0 +1,69 @@
+package org.elsys_bg.mini_booking.property_service;
+
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+
+@Table
+public class PropertyOfUser {
+
+    @PrimaryKeyColumn(
+		name = "user_id", 
+        ordinal = 0, 
+        type = PrimaryKeyType.PARTITIONED
+	) private int userId;
+
+    @PrimaryKeyColumn(
+		name = "property_id", 
+        ordinal = 0, 
+        type = PrimaryKeyType.PARTITIONED
+	) private int propertyId;
+
+	private String propertyTitle;
+	private String propertyDescription;
+	private double propertyPrice;
+
+
+	public PropertyOfUser(int userId,
+						  int propertyId,
+						  String propertyTitle,
+						  String propertyDescription,
+						  double propertyPrice) {
+		this.userId = userId;
+		this.propertyId = propertyId;
+		this.propertyTitle = propertyTitle;
+		this.propertyDescription = propertyDescription;
+		this.propertyPrice = propertyPrice;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public int getPropertyId() {
+		return propertyId;
+	}
+
+
+	public String getPropertyTitle() {
+		return propertyTitle;
+	}
+
+
+	public String getPropertyDescription() {
+		return propertyDescription;
+	}
+
+
+	public double getPropertyPrice() {
+		return propertyPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "PropertyOfUser [userId=" + userId + ", propertyId=" + propertyId + ", propertyTitle=" + propertyTitle
+				+ ", propertyDescription=" + propertyDescription + ", propertyPrice=" + propertyPrice + "]";
+	}
+}
